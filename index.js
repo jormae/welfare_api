61c4a858dc78e7cfb193b075e29709a83fe4eae9
Maheedeen Jormae <maheedeen@gmail.com>
2022-12-06 20:20:09 +0700

    add investment

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const res = require("express/lib/response");
 // const authRouter = require("./routes/auth");
 // const dashboardRouter = require("./routes/dashboard");
 const membersRouter = require("./routes/members");
+const investmentsRouter = require("./routes/investments");
 
 const app = express();
 app.use(cors());
@@ -11,7 +12,7 @@ app.use(express.json());
 
 app.get("/", (req, res) => res.send("Welcome to my js api."));
 // app.use("/auth", authRouter);
-// app.use("/auth/token", authRouter);
 app.use("/members", membersRouter);
+app.use("/investments", investmentsRouter);
 
 app.listen(3030, () => console.log("Server is running on port 3030"));
